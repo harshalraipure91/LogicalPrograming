540e34cd1fa8d2f76d94bc6373e5ed03be5ec9e4
harshalraipure91 <harshalraipure91@gmail.com>
2024-01-21 20:57:30 +0530

    ReverseArray

---
+++ LogicalPrograming/ReverseArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+	internal class ReverseArray
+	{
+		static void Main(string[] args)
+		{
+			int[] array = { 1, 2, 3, 25, 35, 4, 5, 6, 7, 8, 9 };
+
+
+			for (int i = 0; i < array.Length - 1; i++)
+			{
+				for (int j = i + 1; j < array.Length; j++)
+				{
+					var temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+			foreach (var item in array)
+			{
+				Console.Write(item + " ");
+			}
+
+
+			Console.ReadLine();
+		}
+	}
+}
