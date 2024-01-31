6426012cc2a89daa85a6d3cb3202680d4b90780b
harshalraipure91 <harshalraipure91@gmail.com>
2024-01-31 07:19:58 +0530

    FInd SubstringCountIn string

---
+++ LogicalPrograming/FindSubstringCountInString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+	internal class FindSubstringCountInString
+	{
+		static void Main(string[] args)
+		{
+
+			var input = "This is a test message";
+			int count = 0;
+			var subString = "is";
+			var index = input.ToLower().IndexOf(subString);
+
+			while (index > 0)
+			{
+				count++;
+				index = input.ToLower().IndexOf(subString, index + 1);
+			}
+			Console.WriteLine($"No of count of {subString}' is: {count}");
+
+
+
+			Console.ReadLine();
+
+		}
+	}
+}
