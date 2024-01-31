a3eaaa8f6c2e2b8943022d8d82f613aa5d3c5d8e
harshalraipure91 <harshalraipure91@gmail.com>
2024-01-31 08:19:06 +0530

    add second method

--- LogicalPrograming/FindSubstringCountInString.cs
+++ LogicalPrograming/FindSubstringCountInString.cs
@@ -10,17 +10,38 @@ namespace LogicalPrograming
 	{
 		static void Main(string[] args)
 		{
+			#region Method1
+
+			//var input = "This is a test message";
+			//int count = 0;
+			//var subString = "is";
+			//var index = input.ToLower().IndexOf(subString);
+
+			//while (index > 0 || index != -1)
+			//{
+			//	count++;
+			//	index = input.ToLower().IndexOf(subString, index + 1);
+			//}
+			//Console.WriteLine($"No of count of '{subString}' is: {count}");
+
+			#endregion
+
+			#region Method2
+
 			var input = "This is a test message";
 			int count = 0;
 			var subString = "is";
-			var index = input.ToLower().IndexOf(subString);
-
-			while (index > 0 || index != -1)
+			var splitInput = input.ToLower().Split(' ');
+			for (int i = 0; i < splitInput.Length; i++)
 			{
-				count++;
-				index = input.ToLower().IndexOf(subString, index + 1);
+				if (splitInput[i].Contains(subString))
+				{
+					count++;
+				}
 			}
 			Console.WriteLine($"No of count of '{subString}' is: {count}");
+
+			#endregion
 
 			Console.ReadLine();
 
