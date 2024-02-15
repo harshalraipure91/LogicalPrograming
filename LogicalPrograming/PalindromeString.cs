38939982341ba8b283b3dd5cbf6f4668ca84b213
harshalraipure91 <harshalraipure91@gmail.com>
2024-02-15 20:24:42 +0530

    add Palindrome string

---
+++ LogicalPrograming/PalindromeString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+	internal class PalindromeString
+	{
+		static void Main(string[] args)
+		{
+			var input = "madam";
+			var result = string.Empty;
+			for (int i = input.Length - 1; i >= 0; i--)
+			{
+				result += input[i];
+			}
+			Console.WriteLine(input == result ? "Palindrome" : "Non Palindrome");
+		}
+	}
+}
