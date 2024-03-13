638de5965cb3344f00802b57378b3c1be6c58b2b
harshalraipure91 <harshalraipure91@gmail.com>
2024-03-13 10:07:45 +0530

    Add Factoral of number Using Recursive function

--- LogicalPrograming/FactorialNumber.cs
+++ LogicalPrograming/FactorialNumber.cs
@@ -8,19 +8,41 @@ namespace LogicalPrograming
 {
 	internal class FactorialNumber
 	{
+		#region Using For Loop
+		//static void Main(string[] args)
+		//{
+		//	Console.Write("Enter a number: ");
+		//	var number = Convert.ToInt32(Console.ReadLine());
+		//	int fact = 1;
+
+		//	for (int i = 1; i <= number; i++)
+		//	{
+		//		fact *= i;
+		//	}
+		//	Console.WriteLine($"factorial of given number {number} is: {fact}");
+
+		//	Console.ReadLine();
+		//}
+
+		#endregion
+
+
+		#region Using Recursive function
 		static void Main(string[] args)
 		{
 			Console.Write("Enter a number: ");
 			var number = Convert.ToInt32(Console.ReadLine());
-			int fact = 1;
+			var output = GetFactorial(number);
+            Console.WriteLine($"Factorial of number is: {output}");
+        }
 
-			for (int i = 1; i <= number; i++)
-			{
-				fact *= i;
-			}
-			Console.WriteLine($"factorial of given number {number} is: {fact}");
 
-			Console.ReadLine();
+		private static int GetFactorial(int n)
+		{
+			var value = n >= 1 ? n * GetFactorial(n - 1) : 1;
+			return value;
 		}
+
+		#endregion
 	}
 }
