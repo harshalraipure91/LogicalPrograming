ab3b8a03f9b86cb256e8fead6e9611ab86b2c677
harshalraipure91 <harshalraipure91@gmail.com>
2024-03-14 10:23:16 +0530

    Change class name

--- LogicalPrograming/PrintDuplicateNumbrInArray.cs
+++ LogicalPrograming/PrintDuplicateNumbrInArray.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LogicalPrograming
 {
-	internal class GetOnlyDuplicateNumbrInArray
+	internal class PrintDuplicateNumbrInArray
 	{
 		static void Main(string[] args)
 		{
